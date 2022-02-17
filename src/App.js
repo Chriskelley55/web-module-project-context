44822684239040127d407b2d5eea3e703a94863b
Chris Kelley <chriskelley85@gmail.com>
2022-02-17 16:30:14 -0600

    cartcontext in cart and nav

--- src/App.js
+++ src/App.js
@@ -7,31 +7,30 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		setCart({
-			cart: [...cart, item]
-		})
+		setCart([...cart, item])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
+			<CartContext.Provider value={cart} />
+				<Navigation/>
 			{/* Routes */}
 			<ProductContext.Provider value={{products, addItem}}>
 			<Route exact path="/">
 				<Products />
 			</Route>
-
+			</ProductContext.Provider>
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
-			</ProductContext.Provider>
+			
 		</div>
 	);
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -8,7 +8,7 @@ import Product from './Product';
 
 const Products = () => {
 	const {products, addItem} = useContext(ProductContext);
-	console.log(products);
+ 
 
 	return (
 		<div className="products-container">
